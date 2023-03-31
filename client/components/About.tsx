6f101262520eeeec84a9a3b68ec745068bb47c27
aldomarcelino <aldo115marcelino@gmail.com>
2023-03-31 13:09:20 +0700

    make lp about

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -5,8 +5,8 @@ type Props = {};
 
 const About = (props: Props) => {
   return (
-    <div className="flex flex-col h-screen relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute ml-8 top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+    <div className="relative flex flex-col h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
       <motion.img
@@ -16,8 +16,24 @@ const About = (props: Props) => {
         viewport={{ once: true }}
         src="https://images.unsplash.com/photo-1674223761195-76b75ac3490c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=470&q=80"
         alt="aldo-marcelino"
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[500px]"
+        className="-mb-10 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[500px]"
       />
+      <div className="space-y-8 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{" "}
+          <span className="underline decoration-[#F7AB0A]/50">little </span>
+          background
+        </h4>
+        <p className="text-base">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
     </div>
   );
 };
